cc4e3cf8301392c36c899f6851a456520e64d5a2
tugcekarapinar <101087196+tugcekarapinar@users.noreply.github.com>
2024-06-01 21:40:11 +0300

    Üçlü operatör eklendi

--- src/Main.java
+++ src/Main.java
@@ -11,18 +11,17 @@ public class Main {
         System.out.print("Aylık çalıştığınız gün sayısını giriniz:");
         int day = scanner.nextInt();
 
-        if (day<25){
-            double dailySalary = salary / 25;
-            double x = dailySalary * day;
-            System.out.println("Bu ayki kazancınız:" + x + " TL");
-        }
-        else if (day==25) {
-            System.out.println("Bu ayki kazancınız:" + salary + " TL");
-        }
-        else {
-            double extraDay = day-25;
-            double y = salary + (extraDay * 1000);
-            System.out.println("Bu ayki kazancınız:" + y + " TL");
-        }
+        double totalSalary = day<=25 ? CalculateSalary(day,salary) : CalculateSalaryWithPrime(day,salary);
+        System.out.println("Bu ayki kazancınız:" + totalSalary + " TL");
+    }
+
+    public static double CalculateSalary(int day, double salary) {
+        double dailySalary = salary / 25;
+        return dailySalary * day;
+    }
+
+    public static double CalculateSalaryWithPrime(int day, double salary) {
+        double extraDay = day-25;
+        return salary + (extraDay * 1000);
     }
 }
